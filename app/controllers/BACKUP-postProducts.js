e6040d8f862ca78051e9c4928278b83aee3dc4df
Diego K <kleiman.diego@gmail.com>
2024-03-06 08:42:48 -0300

    postProducts.js ya puede cargar productos con categorias multiples

--- app/controllers/BACKUP-postProducts.js
+++ app/controllers/BACKUP-postProducts.js
@@ -1,24 +1,14 @@
 const fs = require("fs");
 const csv = require("csv-parser");
-const path = require("path");
 
 const postProducts = (req, res) => {
   const access_token = req.query.access_token;
   const user_id = req.query.user_id;
-  const prices = [];
-  const SKUs = [];
-  const stock = [];
-  const name = [];
-  const description = [];
-  const categoriesList = [];
-
-  //const costo = [];
+  const products = [];
 
   fs.createReadStream("app/assets/documents/nuevosProductos.csv")
     .pipe(csv({ separator: ";" }))
     .on("data", (data) => {
-      //console.log(data);
-
       if (
         data.SKU &&
         data.NAME &&
@@ -27,62 +17,84 @@ const postProducts = (req, res) => {
         data.CATEGORIES &&
         data.DESCRIPTION
       ) {
-        SKUs.push(data.SKU);
-        prices.push(data.PRICE);
-        stock.push(data.STOCK);
-        name.push(data.NAME);
-        description.push(data.DESCRIPTION);
-        //categoriesList.push(data.CATEGORIES.split(",").map(Number)); //convierto las cat a num entero por la docu
-        const categories = data.CATEGORIES.split(",").map(Number);
-        if (categories.every(Number.isInteger)) {
-          categoriesList.push(categories);
-        } else {
-          console.error("Las categorías no son números enteros:", categories);
-        }
+        const categories = data.CATEGORIES.split(",").map((category) =>
+          parseInt(category.trim())
+        );
+
+        products.push({
+          sku: data.SKU,
+          name: data.NAME,
+          price: parseFloat(data.PRICE),
+          stock: parseInt(data.STOCK),
+          description: data.DESCRIPTION,
+          categories: categories,
+        });
       }
     })
     .on("end", () => {
-      postProductsRecursive(0);
+      postProductsRecursive(0, products, user_id, access_token, res);
     });
-
-  function postProductsRecursive(index) {
-    //segun https://tiendanube.github.io/api-documentation/resources/product
-
-    if (index < SKUs.length) {
-      const categories = categoriesList[index];
-      const body = {
-        name: { es: name[index] },
-        description: { es: description[index] },
-        categories: categories,
-        variants: [
-          {
-            price: prices[index],
-            stock: stock[index],
-            sku: SKUs[index],
-          },
-        ],
-      };
-
-      const endpoint = `https://api.tiendanube.com/v1/${user_id}/products/`;
-
-      fetch(endpoint, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authentication: `bearer ${access_token}`,
-        },
-        body: JSON.stringify(body),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data); // Aquí puedes manejar la respuesta si es necesario
-          setTimeout(() => postProductsRecursive(index + 1), 2000); // Llamada recursiva con el siguiente índice
-        })
-        .catch((error) => console.error(error));
-    } else {
-      res.send("Fin carga de productos"); // Envía la respuesta cuando se hayan procesado todos los productos
-    }
-  }
 };
+
+function postProductsRecursive(index, products, user_id, access_token, res) {
+  if (index < products.length) {
+    const body = {
+      name: { es: products[index].name },
+      description: { es: products[index].description },
+      categories: products[index].categories,
+      variants: [
+        {
+          price: products[index].price,
+          stock: products[index].stock,
+          sku: products[index].sku,
+        },
+      ],
+    };
+
+    const endpoint = `https://api.tiendanube.com/v1/${user_id}/products/`;
+
+    fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authentication: `bearer ${access_token}`,
+      },
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          console.error(
+            "Error en la solicitud:",
+            response.status,
+            response.statusText
+          );
+          return response.text().then((text) => {
+            console.error("Detalles del error:", text);
+            throw new Error(
+              `Error en la solicitud: ${response.status} - ${response.statusText}`
+            );
+          });
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setTimeout(
+          () =>
+            postProductsRecursive(
+              index + 1,
+              products,
+              user_id,
+              access_token,
+              res
+            ),
+          2000
+        );
+      })
+      .catch((error) => console.error(error));
+  } else {
+    res.send("Fin carga de productos");
+  }
+}
 
 module.exports = { postProducts };
--- app/controllers/postProducts.js
+++ app/controllers/postProducts.js
@@ -1,16 +1,10 @@
 const fs = require("fs");
 const csv = require("csv-parser");
-const path = require("path");
 
 const postProducts = (req, res) => {
   const access_token = req.query.access_token;
   const user_id = req.query.user_id;
-  const prices = [];
-  const SKUs = [];
-  const stock = [];
-  const name = [];
-  const description = [];
-  const categories = [];
+  const products = [];
 
   fs.createReadStream("app/assets/documents/nuevosProductos.csv")
     .pipe(csv({ separator: ";" }))
@@ -23,68 +17,84 @@ const postProducts = (req, res) => {
         data.CATEGORIES &&
         data.DESCRIPTION
       ) {
-        SKUs.push(data.SKU);
-        prices.push(data.PRICE);
-        stock.push(data.STOCK);
-        name.push(data.NAME);
-        description.push(data.DESCRIPTION);
-        categories.push(data.CATEGORIES);
+        const categories = data.CATEGORIES.split(",").map((category) =>
+          parseInt(category.trim())
+        );
+
+        products.push({
+          sku: data.SKU,
+          name: data.NAME,
+          price: parseFloat(data.PRICE),
+          stock: parseInt(data.STOCK),
+          description: data.DESCRIPTION,
+          categories: categories,
+        });
       }
     })
     .on("end", () => {
-      postProductsRecursive(0);
+      postProductsRecursive(0, products, user_id, access_token, res);
     });
-
-  function postProductsRecursive(index) {
-    if (index < SKUs.length) {
-      const body = {
-        name: { es: name[index] },
-        description: { es: description[index] },
-        categories: [categories[index]],
-        variants: [
-          {
-            price: prices[index],
-            stock: stock[index],
-            sku: SKUs[index],
-          },
-        ],
-      };
-
-      const endpoint = `https://api.tiendanube.com/v1/${user_id}/products/`;
-
-      fetch(endpoint, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authentication: `bearer ${access_token}`,
-        },
-        body: JSON.stringify(body),
-      })
-        .then((response) => {
-          if (!response.ok) {
-            console.error(
-              "Error en la solicitud:",
-              response.status,
-              response.statusText
-            );
-            return response.text().then((text) => {
-              console.error("Detalles del error:", text);
-              throw new Error(
-                `Error en la solicitud: ${response.status} - ${response.statusText}`
-              );
-            });
-          }
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data); // Aquí puedes manejar la respuesta si es necesario
-          setTimeout(() => postProductsRecursive(index + 1), 2000); // Llamada recursiva con el siguiente índice
-        })
-        .catch((error) => console.error(error));
-    } else {
-      res.send("Fin carga de productos");
-    }
-  }
 };
+
+function postProductsRecursive(index, products, user_id, access_token, res) {
+  if (index < products.length) {
+    const body = {
+      name: { es: products[index].name },
+      description: { es: products[index].description },
+      categories: products[index].categories,
+      variants: [
+        {
+          price: products[index].price,
+          stock: products[index].stock,
+          sku: products[index].sku,
+        },
+      ],
+    };
+
+    const endpoint = `https://api.tiendanube.com/v1/${user_id}/products/`;
+
+    fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authentication: `bearer ${access_token}`,
+      },
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          console.error(
+            "Error en la solicitud:",
+            response.status,
+            response.statusText
+          );
+          return response.text().then((text) => {
+            console.error("Detalles del error:", text);
+            throw new Error(
+              `Error en la solicitud: ${response.status} - ${response.statusText}`
+            );
+          });
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setTimeout(
+          () =>
+            postProductsRecursive(
+              index + 1,
+              products,
+              user_id,
+              access_token,
+              res
+            ),
+          2000
+        );
+      })
+      .catch((error) => console.error(error));
+  } else {
+    res.send("Fin carga de productos");
+  }
+}
 
 module.exports = { postProducts };
