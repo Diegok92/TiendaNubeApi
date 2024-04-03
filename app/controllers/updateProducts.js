ff8ab3fddb7ca475db3b707aafee56f4d6fd0e3c
Diego K <kleiman.diego@gmail.com>
2024-04-03 11:57:29 -0300

    funcionando para mil productos el updateProducts.js, falta modificar la logica de la obtencion de los productos a actualizar

--- app/controllers/updateProducts.js
+++ app/controllers/updateProducts.js
@@ -1,121 +1,74 @@
 const fs = require("fs");
 const csv = require("csv-parser");
 const { createObjectCsvWriter } = require("csv-writer");
-const { performance } = require("perf_hooks"); // Importa el módulo 'perf_hooks' para medir el rendimiento
+const { performance } = require("perf_hooks");
 
-const updateProducts = (req, res) => {
-  const access_token = req.query.access_token;
-  const user_id = req.query.user_id;
+const updateProducts = async (req, res) => {
+  try {
+    const access_token = req.query.access_token;
+    const user_id = req.query.user_id;
 
-  const productosFile = fs.readFileSync(
-    "app/assets/documents/Productos.csv",
-    "utf8"
-  );
-  const nuevosProductosFile = fs.readFileSync(
-    "app/assets/documents/nuevosProductos.csv",
-    "utf8"
-  );
+    const productosActualizados = [];
 
-  const productos = productosFile.split("\n").map((line) => line.split(";"));
-  const nuevosProductos = nuevosProductosFile
-    .split("\n")
-    .map((line) => line.split(";"));
-
-  const productosActualizados = [];
-
-  for (const producto of productos) {
-    const sku = producto[1];
-    const nuevoProducto = nuevosProductos.find((item) => item[0] === sku);
-
-    if (nuevoProducto) {
-      const [id, name, price, stock, categories, description] = nuevoProducto;
-
-      if (
-        producto[4] !== name ||
-        producto[5] !== price ||
-        producto[6] !== stock ||
-        producto[7] !== description
-      ) {
-        productosActualizados.push({
-          ID: producto[0],
-          SKU: producto[1],
-          VARIANT_ID: producto[2],
-          CATEGORIES: categories,
-          NAME: name,
-          PRICE: price,
-          STOCK: stock,
-          DESCRIPTION: description,
-        });
-      }
-    }
-  }
-
-  const actCsvFilePath = "app/assets/documents/actualizarInfoCompleta.csv";
-  const writeHeader = !fs.existsSync(actCsvFilePath);
-
-  const csvWriter = createObjectCsvWriter({
-    path: actCsvFilePath,
-    header: [
-      { id: "ID", title: "ID" },
-      { id: "SKU", title: "SKU" },
-      { id: "VARIANT_ID", title: "VARIANT_ID" },
-      { id: "CATEGORIES", title: "CATEGORIES" },
-      { id: "NAME", title: "NAME" },
-      { id: "PRICE", title: "PRICE" },
-      { id: "STOCK", title: "STOCK" },
-      { id: "DESCRIPTION", title: "DESCRIPTION" },
-    ],
-    fieldDelimiter: ";",
-    append: true,
-    writeHeader: writeHeader,
-  });
-
-  csvWriter.writeRecords(productosActualizados).then(() => {
-    console.log(
-      "Se han actualizados los productos en actualizarInfoCompleta.csv"
-    );
-
-    const csvFilePath = actCsvFilePath;
-    const csvStartTime = new Date();
-    const requestStartTime = performance.now();
-    let contadorProd = 0;
-
-    fs.createReadStream(csvFilePath)
+    // Leer el archivo CSV de actualización
+    fs.createReadStream("app/assets/documents/actualizarInfoCompleta.csv")
       .pipe(csv({ separator: ";" }))
       .on("data", (data) => {
-        const productId = data.ID;
-        const variantId = data.VARIANT_ID;
-        const updatedPrice = parseFloat(data.PRICE);
-        const updatedStock = parseInt(data.STOCK);
+        productosActualizados.push(data);
+      })
+      .on("end", async () => {
+        const csvStartTime = new Date();
+        const requestStartTime = performance.now();
 
-        const body = {
-          price: updatedPrice,
-          stock: updatedStock,
-        };
+        // Procesar cada producto para actualizar
+        for (const producto of productosActualizados) {
+          const productId = producto.ID;
+          const variantId = producto.VARIANT_ID;
+          const updatedPrice = parseFloat(producto.PRICE);
+          const updatedStock = parseInt(producto.STOCK);
 
-        const endpoint = `https://api.tiendanube.com/v1/${user_id}/products/${productId}/variants/${variantId}`;
+          const body = {
+            price: updatedPrice,
+            stock: updatedStock,
+          };
 
-        contadorProd++;
-        updateProductRecursive(endpoint, access_token, body)
-          .then(() =>
-            console.log(`Producto ${contadorProd} actualizado correctamente.`)
-          )
-          .catch((error) =>
+          const endpoint = `https://api.tiendanube.com/v1/${user_id}/products/${productId}/variants/${variantId}`;
+
+          try {
+            let response = await fetchWithRetry(endpoint, {
+              method: "PUT",
+              headers: {
+                "Content-Type": "application/json",
+                Authentication: `bearer ${access_token}`,
+              },
+              body: JSON.stringify(body),
+            });
+
+            if (!response.ok) {
+              throw new Error(
+                `Error en la solicitud: ${response.status} - ${response.statusText}`
+              );
+            }
+
+            console.log(`Producto ${productId} actualizado correctamente.`);
+          } catch (error) {
             console.error(
               `Error al actualizar el producto ${productId}:`,
               error
-            )
-          );
-      })
-      .on("end", async () => {
+            );
+          }
+        }
+
         const requestEndTime = performance.now();
         const requestElapsedTime = requestEndTime - requestStartTime;
+
         console.log(
           `Tiempo total de actualización de productos: ${requestElapsedTime} ms`
         );
 
         const csvEndTime = new Date();
         const csvElapsedTime = csvEndTime - csvStartTime;
+
         console.log(`Tiempo de lectura del archivo CSV: ${csvElapsedTime} ms`);
 
         res
@@ -128,34 +81,28 @@ const updateProducts = (req, res) => {
           .status(500)
           .json({ error: "Ocurrió un error al leer el archivo CSV." });
       });
-  });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Ocurrió un error inesperado." });
+  }
 };
 
-async function updateProductRecursive(endpoint, access_token, body) {
-  try {
-    const response = await fetch(endpoint, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authentication: `bearer ${access_token}`,
-      },
-      body: JSON.stringify(body),
-    });
-
-    if (!response.ok) {
-      throw new Error(
-        `Error en la solicitud: ${response.status} - ${response.statusText}`
-      );
+// Función para realizar una solicitud con reintento en caso de error 429
+const fetchWithRetry = async (url, options, retryDelay = 1000) => {
+  let response;
+  while (true) {
+    response = await fetch(url, options);
+    if (response.status !== 429) {
+      break;
     }
-
-    await sleep(2000); // Pausa de 1 segundo entre solicitudes
-  } catch (error) {
-    throw error;
+    const rateLimitReset = parseInt(response.headers.get("X-Rate-Limit-Reset"));
+    const timeToWait = Math.max(rateLimitReset - Date.now(), retryDelay);
+    console.log(
+      `Esperando ${timeToWait} milisegundos antes de volver a intentarlo...`
+    );
+    await new Promise((resolve) => setTimeout(resolve, timeToWait));
   }
-}
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+  return response;
+};
 
 module.exports = { updateProducts };
